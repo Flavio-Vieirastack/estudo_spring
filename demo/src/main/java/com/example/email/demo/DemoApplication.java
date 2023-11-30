167ef6b20a749182517d73d1291e2195e5e57eb8
Flavio Vieira <flavioemerson1@outlook.com>
2023-11-30 13:20:18 -0300

    comit inicial

--- demo/src/main/java/com/example/email/demo/DemoApplication.java
+++ demo/src/main/java/com/example/email/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.teste.email.email;
+package com.example.email.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/email/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/email/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.teste.email.email;
+package com.example.email.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmailApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
