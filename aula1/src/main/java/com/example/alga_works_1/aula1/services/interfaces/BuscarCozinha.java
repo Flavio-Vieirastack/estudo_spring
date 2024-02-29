5100772573437828d01c067d4e5b3dd35203d85b
Flavio Vieira <flavioemerson1@outlook.com>
2024-02-29 16:50:17 -0300

    adicionado mais arquivos

---
+++ aula1/src/main/java/com/example/alga_works_1/aula1/services/interfaces/BuscarCozinha.java
@@ -0,0 +1,9 @@
+package com.example.alga_works_1.aula1.services.interfaces;
+
+import com.example.alga_works_1.aula1.models.Cozinha;
+
+import java.util.List;
+
+public interface BuscarCozinha {
+    public List<Cozinha> listar();
+}
---
+++ aula1/src/main/java/com/example/alga_works_1/aula1/services/interfaces/BuscarCozinhaImpl.java
@@ -0,0 +1,12 @@
+package com.example.alga_works_1.aula1.services.interfaces;
+
+import com.example.alga_works_1.aula1.models.Cozinha;
+
+import java.util.List;
+
+public class BuscarCozinhaImpl implements  BuscarCozinha{
+    @Override
+    public List<Cozinha> listar() {
+        return null;
+    }
+}
